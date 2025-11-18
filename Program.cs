b686ea687893a2b3aec4c6fb4e170ba9fab47eb4
TMChouys <chouys@gs.takming.edu.tw>
2025-11-18 14:44:58 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode_326
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer to check if it is a power of three:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(IsPowerOfThree(a) ? "是 3 的次方" : "不是 3 的次方");
+        }
+
+        static bool IsPowerOfThree(int n)
+        {
+            do
+            {
+                if (n <= 0)
+                {
+                    return false;
+                }
+                else if (n == 1)
+                {
+                    return true;
+                }
+                else if (n % 3 != 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    n = n / 3;
+                }
+            } while (true);
+        }
+    }
+}
